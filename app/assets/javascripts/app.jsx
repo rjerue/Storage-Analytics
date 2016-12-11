554e5e69365d931d246ccae07a7d5042a0c7be55
Ryan Jerue <ryan@jerue.org>
2016-12-11 18:25:21 -0500

    added chart

--- app/assets/javascripts/app.jsx
+++ app/assets/javascripts/app.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Gcomponent from './Gcomponent.jsx'
 import '../stylesheets/style.scss';
 
 ReactDOM.render((
     <div>
         <h1>Playframework, React JS, ES 6 and webpack</h1>
         <h2>Tom Brady did not deflate those footballs!</h2>
+				<Gcomponent/>
     </div>), document.getElementById("app"));
