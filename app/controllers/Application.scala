b5f11aeac3d39b55ba1cfdf846830f6b12af9995
Ryan Jerue <ryan@jerue.org>
2016-12-19 01:37:24 -0500

    xhr is working.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,6 +5,11 @@ import play.api.mvc._
 
 class Application extends Controller {
   def index = Action {
-    Ok(views.html.app("Your new application is ready."))
+    Ok(views.html.app("HP Storage Analytics Application"))
   }
+
+	def tname = Action{
+		Ok("HP Storage Analytics Application")
+	}
+
 }
