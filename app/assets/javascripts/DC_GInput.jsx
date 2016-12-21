0d1193f8214cdcb22febdbba61dfab5fa9dcb702
Ryan Jerue <ryan@jerue.org>
2016-12-21 02:40:59 -0500

    it works fam

--- app/assets/javascripts/DC_GInput.jsx
+++ app/assets/javascripts/DC_GInput.jsx
@@ -4,21 +4,37 @@ const bStyle = {
     'marginTop': '25px'
 };
 
-
-
 class DC_GInput extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            value: ''
+        }
+				this.handleChange = this.handleChange.bind(this);
+				this.handleClick = this.handleClick.bind(this);
     }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        console.log('The button was clicked.');
+				console.log("hostname " + window.location.hostname)
+				console.log("href " + 	window.location.href)
+				window.location.href = "/" + this.state.value
+    }
+
     render() {
         return (
             <div className="row">
                 <div className="col-md-9 col-md-offset-1">
                     <label for="usr">ServerID:</label>
-                    <input type="text" className="form-control" id="svr"/>
+                    <input type="text" className="form-control" id="svr" value={this.state.value} onChange={this.handleChange}/>
                 </div>
                 <div className="col-md-1">
-                    <Button type="button" className="btn btn-primary" style={bStyle}>OK</Button>
+                    <Button type="button" className="btn btn-primary" style={bStyle} onClick={this.handleClick}>OK</Button>
                 </div>
             </div>
         );
